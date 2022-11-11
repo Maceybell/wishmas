c3f713e5b273050e87125fd7607a7ba591237f05
Macey Bell <maceytbell@gmail.com>
2022-11-10 17:14:41 -0700

    Starting event listeners, dropdown hover weird

---
+++ gift-guides.js
@@ -0,0 +1,32 @@
+let forHimBtn = document.querySelector('.him')
+let forHerBtn = document.querySelector('.her')
+let forTeensBtn = document.querySelector('.teens')
+let forKidsBtn = document.querySelector('.kids')
+let forFurBtn = document.querySelector('.fur')
+
+
+ function showForHim (){
+
+ }
+
+ function showForHer (){
+
+ }
+ function showForTeens (){
+
+ }
+ function showForKids (){
+
+ }
+ function showForFur (){
+
+ }
+
+
+forHimBtn.addEventListener('click', showForHim)
+forHerBtn.addEventListener('click', showForHer)
+forTeensBtn.addEventListener('click', showForHim)
+forKidsBtn.addEventListener('click', showForHim)
+forFurBtn.addEventListener('click', showForHim)
+
+
